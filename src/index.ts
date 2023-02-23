1a39027b85548feacb84392b2281f5a9cd29b321
Cairo <cairocampos98@gmail.com>
2023-02-23 09:26:06 -0300

    feat: add search term and prefix

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log('Hello')
+import readline from 'readline-sync'
+import { Content } from './types/content';
+
+function start() {
+  const content: Content = {
+    searchTerm: "",
+    prefix: "",
+  }
+
+  content.searchTerm = askAndReturnSearchTerm();
+  content.prefix = askAndReturnPrefix();
+
+  function askAndReturnSearchTerm() {
+    return readline.question('Digite um termo de busca da Wikipedia: ');
+  }
+
+  function askAndReturnPrefix() {
+    const prefixes = ['Quem é', 'O que é', 'A história de']
+    const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Escolha uma opção')
+    const selectedPrefixText = prefixes[selectedPrefixIndex];
+
+    return selectedPrefixText;
+  }
+}
+
+start();
